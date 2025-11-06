69b554f682230c38d39eab8bcefbf4b7831482bd
Sunmi-CS <sunmicasano@gmail.com>
2025-11-05 22:26:39 -0500

    Se edito el app.jsx

--- countries-react/src/App.jsx
+++ countries-react/src/App.jsx
@@ -1,11 +1,26 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Countries from "./pages/Countries";
 
 function App() {
-
   return (
     <>
-      
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <Link className="navbar-brand" to="/">🌍 World Explorer</Link>
+          <div>
+            <Link className="nav-link d-inline text-white mx-2" to="/">Home</Link>
+            <Link className="nav-link d-inline text-white mx-2" to="/countries">Countries</Link>
+          </div>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/countries" element={<Countries />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
